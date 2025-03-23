5a54c786c6b0c7603c5e98d7f7f502e42c435bce
ruslanpavliichuk <pavliichukruslan@gmail.com>
2025-03-23 16:29:19 +0000

    Added a nav bar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+};
